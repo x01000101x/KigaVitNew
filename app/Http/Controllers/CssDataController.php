76294ec0b1ab16bc35d916847e6fe083b2b1e3ad
x01000101x <leodamanik619@gmail.com>
2022-06-06 15:32:05 +0700

    + css_datas table

---
+++ app/Http/Controllers/CssDataController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Css_data;
+use Illuminate\Http\Request;
+
+class CssDataController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Css_data  $css_data
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Css_data $css_data)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Css_data  $css_data
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Css_data $css_data)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Css_data  $css_data
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Css_data $css_data)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Css_data  $css_data
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Css_data $css_data)
+    {
+        //
+    }
+}
