e15e288b0024e4b946c708e330687b8e3bbc4b70
x01000101x <leodamanik619@gmail.com>
2022-06-06 15:22:48 +0700

    re-migrate + category table

--- database/migrations/2022_06_06_081756_create_categories_table.php
+++ database/migrations/2022_06_06_081756_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBookmarksTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateBookmarksTable extends Migration
      */
     public function up()
     {
-        Schema::create('bookmarks', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
-            $table->unsignedBigInteger('template_id');
-            $table->foreign('template_id')->references('id')->on('templates');
+            $table->string('category');
+            $table->string('slug');
+            $table->text('description');
+            $table->text('thumb');
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreateBookmarksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('bookmarks');
+        Schema::dropIfExists('categories');
     }
 }
